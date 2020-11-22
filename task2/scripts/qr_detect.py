7fb022c32c21c34fbb3edd8faa72a1235790a27d
hashmis79 <hashmis79@yahoo.com>
2020-11-22 14:37:35 +0530

    Added comments

--- task2/scripts/qr_detect.py
+++ task2/scripts/qr_detect.py
@@ -17,6 +17,25 @@ import pyzbar.pyzbar as pyzbar
 import rospy
 
 class image_proc():
+	"""
+    This is the main class for Vitarana E-Drone.
+    It handles
+    ----------
+        1) Initialization of variables
+        2) Decoding the QR Image from the camera and publishing the decoded Data
+        3) Callbacks for image conversion
+        4) Basic Tasks like initalizing a node, Publishing and Subscribing
+
+
+    Methods :
+    ----------
+	ScanCode:
+		Function for Scanning the image, decoding the found QR code and publishing the Decoded coordinates
+
+	Image Callback:
+		 Callback function for converting the recieved Raw image from the camera to bgr8 image
+
+    """
 
 	# Initialise everything
 	def __init__(self):
@@ -30,15 +49,21 @@ class image_proc():
 
 
 	def ScanCode(self):
+		#Checking if image is a non null value
 		if len(self.img.shape) > 1:
+			#Decoding the image for finding the QR
 			self.decodedObjects = pyzbar.decode(self.img)
 			for obj in self.decodedObjects:
 				stringSlice = str.split(obj.data, ",")
-				self.list_custom.longitude = float(stringSlice[0])
-				self.list_custom.latitude = float(stringSlice[1])
-				self.list_custom.altitude = float(stringSlice[2])
-				self.list_custom.scan = True
-				self.location_pub.publish(self.list_custom)
+				try:
+					self.list_custom.longitude = float(stringSlice[0])
+					self.list_custom.latitude = float(stringSlice[1])
+					self.list_custom.altitude = float(stringSlice[2])
+					self.list_custom.scan = True
+					#Publishing the New found coordinates fr the QR code
+					self.location_pub.publish(self.list_custom)
+				except :
+					pass
 				print("Data", stringSlice[0])
 				print("Data", stringSlice[1])
 				print("Data", stringSlice[2])
