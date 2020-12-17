d1db25fa7242361bad5e53560cb104aec1ff8c0f
SAtacker <shreyasatre16@gmail.com>
2020-12-18 01:13:54 +0530

    Added Marker detection

---
+++ task3/scripts/detection.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+import cv2
+from matplotlib import pyplot as plt
+import math
+import rospy
+from std_msgs.msg import Float32
+from sensor_msgs.msg import Image,NavSatFix
+from cv_bridge import CvBridge, CvBridgeError
+import numpy as np
+
+class Marker():
+    """
+    Marker Detection Object : Publishes X,Y error in meters
+    """
+
+    def __init__(self):
+        self.hfov_rad = 1.3962634
+        self.err_x_m = 0.0
+        self.err_y_m = 0.0
+        self.bridge = CvBridge()
+        self.logo_cascade = cv2.CascadeClassifier('cascade.xml')
+        self.img = np.empty([])
+        self.logo = None
+        self.center_of_box = [0.0,0.0]
+        self.Z_m = -1.0
+        # Subscribing to the camera topic
+        rospy.Subscriber("/edrone/camera/image_raw",Image, self.image_callback)
+        rospy.Subscriber('/edrone/gps',NavSatFix, self.gps_callback)
+        self.x_err_pub = rospy.Publisher("/edrone/err_x_m",data_class=Float32,queue_size=1)
+        self.y_err_pub = rospy.Publisher("/edrone/err_y_m",data_class=Float32,queue_size=1)
+    
+    def gps_callback(self, msg):
+        self.Z_m = msg.altitude
+
+    # Callback function of camera topic
+    def image_callback(self, data):
+        try:
+            self.img = self.bridge.imgmsg_to_cv2(data, "bgr8")
+            self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+        except:
+			print("Error")
+			return
+
+    def detect(self):
+        self.logo = self.logo_cascade.detectMultiScale(self.img, scaleFactor=1.05)
+        (x, y, w, h) = self.logo
+        self.center_of_box = (w/2+x), (h/2+y)
+        self.err_x_m = self.meter_from_pix(self.center_of_box[0],0)
+        self.err_y_m = self.meter_from_pix(self.center_of_box[1],1)
+    
+    def focal_length(self,i):
+        return (img.shape[i]/2)/math.tan(hfov_rad/2)
+
+    def meter_from_pix(self,pix,i):
+        return (self.img.shape[i]/2-pix)*self.Z_m/self.focal_length(i)
+
+    def pub(self):
+        self.x_err_pub.publish(self.err_x_m)
+        self.y_err_pub.publish(self.err_y_m)
+
+if __name__ == "__main__" :
+    marker = Marker()
+    rate = rospy.Rate(1)
+    while not rospy.is_shutdown():
+        marker.detect()
+        marker.pub()
+        rate.sleep()
---
+++ task3/scripts/handle_states.py
@@ -0,0 +1,17 @@
+states = ["takeoff","mid_air_no_obstacle","mid_air_obstacle","landing"]
+
+def controller(state):
+    if state == states[0]:
+        None
+    elif state == states[1]:
+        None
+    elif state == states[2]:
+        None
+    elif state == states[3]:
+        None
+
+def check_states(takeoff_location,target_location,current_location,range_finder_readings):
+    """
+        takeoff_location
+    """
+    None
